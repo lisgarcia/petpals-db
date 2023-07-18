f72c33fa7254b7fda85bdc84950a1242aa5cbeec
Mai <garcia.ton94@gmail.com>
2023-07-18 17:19:17 -0400

    removed pets and meetups

--- delete.py
+++ delete.py
@@ -5,11 +5,11 @@ from models import db, Pet, User, Meetup
 # with app.app_context():
 # Info on application context: https://flask.palletsprojects.com/en/1.1.x/appcontext/
 with app.app_context():
-    # pets = Pet.query.all()
+    pets = Pet.query.all()
     meetups = Meetup.query.all()
 
-    # for pet in pets:
-    #     db.session.delete(pet)
+    for pet in pets:
+        db.session.delete(pet)
 
     for meetup in meetups:
         db.session.delete(meetup)
