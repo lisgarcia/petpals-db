6f2c16a55370e1f009a5bed322cb3da7f82f0e0c
Mai <garcia.ton94@gmail.com>
2023-07-18 17:18:11 -0400

    meetups and serialize rles

--- delete.py
+++ delete.py
@@ -5,12 +5,12 @@ from models import db, Pet, User, Meetup
 # with app.app_context():
 # Info on application context: https://flask.palletsprojects.com/en/1.1.x/appcontext/
 with app.app_context():
-    pets = Pet.query.all()
-    # meetups = Meetup.query.all()
+    # pets = Pet.query.all()
+    meetups = Meetup.query.all()
 
-    for pet in pets:
-        db.session.delete(pet)
+    # for pet in pets:
+    #     db.session.delete(pet)
 
-    # for meetup in meetups:
-    #     db.session.delete(meetup)
+    for meetup in meetups:
+        db.session.delete(meetup)
     db.session.commit()
